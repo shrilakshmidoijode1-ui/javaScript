348eda645469f128d2328157cea013ecaaa4d20b
shrilakshmidoijode1-ui <shrilakshmidoijode1@gmail.com>
2026-04-20 14:03:30 +0000

    added datatypes and variables

---
+++ JS_Mastery/01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+var AccountPass = "12345"
+let email = "shri@gmail.com"
+const id = 124420
+
+let name = "laxmi";
+let usn = "1nh24cs420"; // Wrapped in quotes 
+let clg = false;        // No question mark
+let breakfast;
+let assignment = 0;
+
+console.table([AccountPass, email, id, usn, assignment, clg]);
